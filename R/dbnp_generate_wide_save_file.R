801f42d1a7ca0c5f90f47ba5f36e22f607ac17df
Finn Sandø <fs@3q.dk>
2017-05-02 19:30:51 +0200

    Optimize generator function (almost 3x speed up)

--- R/dbnp_generate_wide_save_file.R
+++ R/dbnp_generate_wide_save_file.R
@@ -1,13 +1,25 @@
-#' Generate special format data file for upload to dbNp Phenotyp Database
+#' Generate file specifically formatted for import into dbNp Phenotype Database
 #' @export
-dbnp_generate_wide_save_file <- function(x,id_var,treat_var,visit_var,time_var,visit_units="weeks",time_units="minutes",exclude_vars=NULL,file="",sep=",",dec=".",rename_id=TRUE) {
-
+dbnp_generate_wide_save_file <- function(
+      x
+      ,id_var
+      ,treat_var
+      ,visit_var
+      ,time_var
+      ,visit_units="weeks"
+      ,time_units="minutes"
+      ,include_vars=NULL
+      ,exclude_vars=NULL
+      ,file=""
+      ,sep=","
+      ,dec="."
+      ,rename_id=TRUE
+  ) {
+  
   visits <- sort(unique(x[,visit_var]))
   treatments <- sort(unique(x[,treat_var]))
   times <- sort(unique(x[,time_var]))
   ids <- sort(unique(x[,id_var]))
-  
-  # x <- x[order(x[,id_var],x[,treat_var],x[,visit_var],x[,time_var]),]
   
   ## make sure design variables are always excluded
   design_vars <- c(id_var,treat_var,visit_var,time_var)
@@ -16,11 +28,14 @@ dbnp_generate_wide_save_file <- function(x,id_var,treat_var,visit_var,time_var,v
   } else {
     exclude_vars <- unique(c(exclude_vars,design_vars))
   }
-  print(design_vars)
-  print(exclude_vars)
+
   ## get names of measurement variables
   measure_varnames <- names(x)[which(!(names(x) %in% exclude_vars))]
-  print(measure_varnames)
+  
+  ## filter out variables not in include_vars
+  if(is.character(include_vars)) {
+    measure_varnames <- measure_varnames[which(measure_varnames %in% include_vars)]
+  }
   
   ## compute time names from visits and times
   visits_times <- sort(rep(visits,length(times)))
@@ -29,26 +44,32 @@ dbnp_generate_wide_save_file <- function(x,id_var,treat_var,visit_var,time_var,v
   ## combine visits and times into one set of times
   x[,"__INTERNAL__time_names"] <- sapply(time_converter(x[,visit_var],visit_units) + time_converter(x[,time_var],time_units),time_create)
 
-  ## wide format number of columns: (visits_times) * treatments * measurements + subject_id
+  ## wide format number of columns: visits_times * treatments * measurements + subject_id
   ncol <- length(time_names) * length(measure_varnames) * length(treatments) + 1
-  
-  df <- as.data.frame(matrix(NA, nrow=1,ncol=ncol))
-  
+  ## number of rows (1 row per subject)
+  nrow <- length(ids)
+
   ## debug: print treatments
   for(tr in 1:length(treatments)) {
     cat("  Treatment: ",tr," ",treatments[tr],"\n")
   }
+
+  ## initialize idx-vars
+  vt <- 1
+  m <- 1
+  row <- 1
   
   ## empty data.frame to collect restructured data
-  out_df <- as.data.frame(matrix(nrow=0,ncol=ncol))
+  out_df <- as.data.frame(matrix(NA,nrow=nrow,ncol=ncol))
+  
+  ##------------------------------------------------------------------
+  ## loop starts
+  ##------------------------------------------------------------------
   for(id in ids) {
-    
-    ## temporary data.frame (colect data from a single subject)
-    tmp_df <- df
     
     ## position in temp df
     pos <- 1
-    tmp_df[,pos] <- id
+    out_df[row,pos] <- id
     pos <- pos + 1
 
     ## treatments
@@ -57,19 +78,20 @@ dbnp_generate_wide_save_file <- function(x,id_var,treat_var,visit_var,time_var,v
       ## extract measurements for single id,treatment
       tmp_dat <- x[which(x[,id_var] == id & x[,treat_var] == treatments[tr]),]
       
+      ## print out during execution
+      cat("\rid: ",id,', treat: ',tr,sep="")
+      
       ## measurements
       for(m in 1:length(measure_varnames)) {
         for(vt in 1:length(time_names)) {
           if(length(idx <- which(tmp_dat[,"__INTERNAL__time_names"] == time_names[vt]))) {
-            tmp_df[,pos] <- tmp_dat[idx,measure_varnames[m]]
-          } else {
-            tmp_df[,pos] <- NA
-          }
+            out_df[row,pos] <- tmp_dat[idx,measure_varnames[m]]
+          } 
           pos <- pos + 1
         }
       }
     }
-    out_df <- rbind(out_df,tmp_df)
+    row <- row + 1
   }
   
   ## create headers
